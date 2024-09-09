e1cc50c902dd6f72e2cad4cc339d3bb88b79b8e6
boobaGreen <claudiodallara77@gmail.com>
2024-09-09 02:55:20 +0200

    quest test 2

--- campaigns/token-standards/building-erc-20/contracts/MintableDoubloon.sol
+++ campaigns/token-standards/building-erc-20/contracts/MintableDoubloon.sol
@@ -3,8 +3,33 @@ pragma solidity ^0.8.20;
 
 import "./Doubloon.sol";
 
+/**
+ * @dev Contratto che eredita da Doubloon e aggiunge funzionalità di minting.
+ */
 contract MintableDoubloon is Doubloon {
+    address public owner;
 
-    constructor(uint256 _supply) Doubloon(_supply) {}
+    /**
+     * @dev Costruttore che imposta il creatore del contratto come proprietario.
+     * @param _supply La quantità iniziale di token da creare.
+     */
+    constructor(uint256 _supply) Doubloon(_supply) {
+        owner = msg.sender; // Imposta il creatore del contratto come proprietario.
+    }
 
+    /**
+     * @dev Funzione per creare nuovi token.
+     * Può essere chiamata solo dal proprietario del contratto.
+     * @param _to Indirizzo a cui verranno assegnati i nuovi token.
+     * @param _amount Quantità di token da creare.
+     */
+    function mint(address _to, uint256 _amount) external {
+        require(
+            msg.sender == owner,
+            "Solo il proprietario può mintare nuovi token"
+        );
+        totalSupply += _amount; // Aumenta la fornitura totale.
+        balanceOf[_to] += _amount; // Aggiunge i nuovi token al bilancio dell'indirizzo _to.
+        emit Transfer(address(0), _to, _amount); // Emissione evento Transfer con from impostato a zero address.
+    }
 }

6efc73eeba2ead62079438b178385a7e83eb9d00
boobaGreen <claudiodallara77@gmail.com>
2024-09-09 23:03:58 +0200

    erc 21 part 1 final

--- campaigns/token-standards/building-erc-721/contracts/Amulet.sol
+++ campaigns/token-standards/building-erc-721/contracts/Amulet.sol
@@ -9,14 +9,17 @@ import "./interfaces/IERC721TokenReceiver.sol";
 /**
  * @dev ERC-721 token contract.
  */
-contract Amulet /* is IERC721, IERC721Metadata */ {
+/* is IERC721, IERC721Metadata */ contract Amulet {
     string public constant name = "Amulet";
     string public constant symbol = "AMULET";
 
     address private immutable creator;
     mapping(uint => string) private uris;
     uint256 private nextId;
-    
+
+    mapping(address => uint) balance;
+    mapping(uint => address) owner;
+
     constructor() {
         creator = msg.sender;
     }
@@ -24,5 +27,34 @@ contract Amulet /* is IERC721, IERC721Metadata */ {
     function tokenURI(uint256 _tokenId) external view returns (string memory) {
         require(_tokenId < nextId, "Invalid token");
         return uris[_tokenId];
+    }
+
+    function balanceOf(address _owner) external view returns (uint256) {
+        require(_owner != address(0), "Invalid address!");
+        return balance[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        require(_tokenId < nextId, "Invalid token!");
+        return owner[_tokenId];
+    }
+
+    function mint(
+        address _owner,
+        string calldata _uri
+    ) external returns (uint256) {
+        require(msg.sender == creator, "Only creator can mint!");
+        owner[nextId] = _owner;
+        balance[_owner]++;
+        uris[nextId] = _uri;
+
+        return nextId++;
+    }
+    function supportsInterface(
+        bytes4 interfaceId
+    ) external pure returns (bool) {
+        return (interfaceId == type(IERC165).interfaceId ||
+            interfaceId == type(IERC721).interfaceId ||
+            interfaceId == type(IERC721Metadata).interfaceId);
     }
 }

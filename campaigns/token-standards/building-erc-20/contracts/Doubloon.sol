5cc14c12c448406aa1c52855bb58542bceffe34b
boobaGreen <claudiodallara77@gmail.com>
2024-09-09 02:37:45 +0200

    quest test 2

--- campaigns/token-standards/building-erc-20/contracts/Doubloon.sol
+++ campaigns/token-standards/building-erc-20/contracts/Doubloon.sol
@@ -8,6 +8,8 @@ contract Doubloon {
     string public constant name = "Doubloon";
     string public constant symbol = "DBL";
     uint256 public totalSupply; // Variabile di stato per la fornitura totale di token.
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     /**
      * @dev Il costruttore inizializza la fornitura totale dei token.
@@ -15,5 +17,48 @@ contract Doubloon {
      */
     constructor(uint256 _supply) {
         totalSupply = _supply; // Imposta la fornitura totale.
+        balanceOf[msg.sender] = _supply; // Assegna tutti i token al creatore iniziale.
+    }
+
+    function transfer(address _to, uint256 _value) external returns (bool) {
+        require(balanceOf[msg.sender] >= _value, "Saldo insufficiente");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        return true;
+    }
+
+    // This is implicitly declared!
+    function allowance(
+        address _owner,
+        address _spender
+    ) external view returns (uint256) {
+        return allowance[_owner][_spender];
+    }
+
+    function approve(
+        address _spender,
+        uint256 _amount
+    ) external returns (bool) {
+        allowance[msg.sender][_spender] = _amount;
+        return true;
+    }
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) external returns (bool) {
+        require(balanceOf[_from] >= _amount, "Insufficient funds");
+
+        require(
+            allowance[_from][msg.sender] >= _amount,
+            "Insufficient allowance"
+        );
+
+        allowance[_from][msg.sender] -= _amount;
+
+        balanceOf[_from] -= _amount;
+        balanceOf[_to] += _amount;
+
+        return true;
     }
 }

01c3e0368dea9db6b0548923a30ed05c07bf84ab
boobaGreen <claudiodallara77@gmail.com>
2024-09-09 22:52:46 +0200

    erc 21 part 1

--- campaigns/token-standards/building-erc-721/contracts/Amulet.sol
+++ campaigns/token-standards/building-erc-721/contracts/Amulet.sol
@@ -10,5 +10,19 @@ import "./interfaces/IERC721TokenReceiver.sol";
  * @dev ERC-721 token contract.
  */
 contract Amulet /* is IERC721, IERC721Metadata */ {
+    string public constant name = "Amulet";
+    string public constant symbol = "AMULET";
 
+    address private immutable creator;
+    mapping(uint => string) private uris;
+    uint256 private nextId;
+    
+    constructor() {
+        creator = msg.sender;
+    }
+
+    function tokenURI(uint256 _tokenId) external view returns (string memory) {
+        require(_tokenId < nextId, "Invalid token");
+        return uris[_tokenId];
+    }
 }

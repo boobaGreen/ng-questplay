ae10a631c1daafe5cfab367d45fee050ea676ef4
boobaGreen <claudiodallara77@gmail.com>
2024-09-12 01:33:21 +0200

    melody2

--- campaigns/understanding-storage/storage-efficiency/contracts/Elegy2.sol
+++ campaigns/understanding-storage/storage-efficiency/contracts/Elegy2.sol
@@ -2,19 +2,35 @@
 pragma solidity ^0.8.19;
 
 contract Elegy2 {
+    // ORIGINAL
 
-    uint32[] public lines;
-    uint public totalSum;
+    //uint32[] public lines;
+    //uint public totalSum;
+
+    uint32[] public lines; // 32 bytes o 256 bits per [0] dove ce la lunghezza
+    uint public totalSum; //32 bytes o 256 bits
 
     constructor(uint32[] memory _lines) {
         lines = _lines;
     }
 
-    function play(uint nonce) external {
-        totalSum = 0;
-        for(uint i = 0; i < lines.length; i++) {
-            totalSum += (i * nonce) * lines[i];
-        }
-    }
+    // function play(uint nonce) external {
+    //     totalSum = 0;
+    //     for (uint i = 0; i < lines.length; i++) {
+    //         totalSum += (i * nonce) * lines[i];
+    //     }
+    // }
 
+    // SOLUTION
+
+    function play(uint nonce) external {
+        uint _totalSum = 0; // Variabile locale per evitare di accedere spesso a totalSum nello storage
+        uint _length = lines.length; // Caching della lunghezza dell'array in memoria
+
+        for (uint i = 0; i < _length; i++) {
+            _totalSum += i * nonce * lines[i]; // Riduzione delle operazioni
+        }
+
+        totalSum = _totalSum; // Aggiornamento della variabile di stato una sola volta alla fine
+    }
 }

b6d1f9c6786bae50ed7c0be570b1a720f61f4fb3
boobaGreen <claudiodallara77@gmail.com>
2024-09-12 01:05:52 +0200

    melody1

--- campaigns/understanding-storage/storage-efficiency/contracts/Elegy1.sol
+++ campaigns/understanding-storage/storage-efficiency/contracts/Elegy1.sol
@@ -2,14 +2,25 @@
 pragma solidity ^0.8.19;
 
 contract Elegy1 {
+    // slot da 256 bits o 32 bytes
 
-    bytes8 public firstVerse;
-    bytes32 public secondVerse;
-    address public thirdVerse;
-    uint128 public fourthVerse;
-    uint96 public fifthVerse;
+    // ORIGINALS
 
-    function setVerse (
+    // bytes8 public firstVerse; // 8 bytes o 64 bits
+    // bytes32 public secondVerse; //32 bytes o 256 bits
+    // address public thirdVerse; // 20 bytes o 160 bits
+    // uint128 public fourthVerse; // 16 bytes o 128 bits
+    // uint96 public fifthVerse; // 12 bytes o 96 bits
+
+    // SOLUTIONS
+    
+    bytes32 public secondVerse; // 32 bytes
+    address public thirdVerse; // 20 bytes
+    uint96 public fifthVerse; // 12 bytes
+    uint128 public fourthVerse; // 16 bytes
+    bytes8 public firstVerse; // 8 bytes
+
+    function setVerse(
         bytes8 _firstVerse,
         bytes32 _secondVerse,
         address _thirdVerse,
@@ -22,5 +33,4 @@ contract Elegy1 {
         fourthVerse = _fourthVerse;
         fifthVerse = _fifthVerse;
     }
-
 }

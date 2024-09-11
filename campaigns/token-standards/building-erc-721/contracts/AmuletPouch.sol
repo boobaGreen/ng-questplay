7cb5ff55c11c182f574960a27268aaa7619c8cbd
boobaGreen <claudiodallara77@gmail.com>
2024-09-11 12:56:36 +0200

    finally!

--- campaigns/token-standards/building-erc-721/contracts/AmuletPouch.sol
+++ campaigns/token-standards/building-erc-721/contracts/AmuletPouch.sol
@@ -1,98 +1,103 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
-import "./interfaces/IERC721.sol";
+import "hardhat/console.sol";
 import "./interfaces/IAmuletPouch.sol";
+import "./Amulet.sol";
 
-/**
- * @dev ERC-721 Token Receiver token contract.
- */
-/* is IAmuletPouch */ contract AmuletPouch {
-    address private immutable amuletContract;
-    mapping(address => bool) private members;
-    mapping(uint256 => Request) private requests;
-    uint256 private memberCount;
-    mapping(uint256 => mapping(address => bool)) private votes;
+contract AmuletPouch is IAmuletPouch {
+    Amulet public immutable amulet;
+    address[] public members;
     uint256 private nextRequestId;
 
-    struct Request {
+    struct WithdrawRequest {
         address requester;
         uint256 tokenId;
         uint256 votes;
+        bool completed;
     }
+
+    mapping(address => bool) public isMemberMap;
+    mapping(uint256 => WithdrawRequest) public withdrawRequests;
+    mapping(address => mapping(uint256 => bool)) public hasVoted;
 
     constructor(address _amulet) {
-        amuletContract = _amulet;
+        amulet = Amulet(_amulet);
     }
 
-    event WithdrawRequested(
-        address indexed requester,
-        uint256 indexed tokenId,
-        uint256 indexed requestId
-    );
-
-    function isMember(address _user) external view returns (bool) {
-        return members[_user];
+    // Return whether the user is a member of the pouch
+    function isMember(address _user) external view override returns (bool) {
+        return isMemberMap[_user];
     }
 
-    function onERC721Received(
-        address /* _operator */,
-        address _from,
-        uint256 _tokenId,
-        bytes calldata _data
-    ) external returns (bytes4) {
-        require(msg.sender == amuletContract, "Only Amulet can be accepted");
-
-        if (!members[_from]) {
-            members[_from] = true;
-            memberCount++;
-        } else if (_data.length > 0) {
-            uint256 requestId = uint256(bytes32(_data));
-            requests[nextRequestId++] = Request(_from, _tokenId, 1);
-            emit WithdrawRequested(_from, _tokenId, requestId);
-        }
-
-        return this.onERC721Received.selector;
+    // Return the total number of members
+    function totalMembers() external view override returns (uint256) {
+        return members.length;
     }
 
+    // Return details of a specific withdraw request
     function withdrawRequest(
         uint256 _requestId
-    ) external view returns (address, uint256) {
-        Request storage request = requests[_requestId];
+    ) external view override returns (address, uint256) {
+        WithdrawRequest memory request = withdrawRequests[_requestId];
         return (request.requester, request.tokenId);
     }
 
-    function numVotes(uint256 _requestId) external view returns (uint256) {
-        return requests[_requestId].votes;
+    // Return the number of votes for a specific request
+    function numVotes(
+        uint256 _requestId
+    ) external view override returns (uint256) {
+        return withdrawRequests[_requestId].votes;
     }
 
-    function totalMembers() external view returns (uint256) {
-        return memberCount;
+    // Vote for a withdraw request
+    function voteFor(uint256 _requestId) external override {
+        require(isMemberMap[msg.sender], "Not a member");
+        require(!hasVoted[msg.sender][_requestId], "Already voted");
+        require(_requestId < nextRequestId, "Request does not exist");
+
+        WithdrawRequest storage request = withdrawRequests[_requestId];
+        request.votes++;
+        hasVoted[msg.sender][_requestId] = true;
     }
 
-    function voteFor(uint256 _requestId) external {
-        require(members[msg.sender], "Not a member");
-        require(!votes[_requestId][msg.sender], "Already voted");
-        require(
-            requests[_requestId].requester != address(0),
-            "Request does not exist"
-        );
+    // Withdraw the amulet if majority vote is reached
+    function withdraw(uint256 _requestId) external override {
+        WithdrawRequest storage request = withdrawRequests[_requestId];
 
-        votes[_requestId][msg.sender] = true;
-        requests[_requestId].votes++;
-    }
-
-    function withdraw(uint256 _requestId) external {
-        Request storage request = requests[_requestId];
-        require(request.votes + 1 > memberCount / 2, "Not enough votes");
         require(request.requester == msg.sender, "Not the requester");
+        require(request.votes > members.length / 2, "Insufficient votes");
+        require(!request.completed, "Request already processed");
 
-        IERC721(amuletContract).transferFrom(
-            address(this),
-            msg.sender,
-            request.tokenId
-        );
+        request.completed = true;
+        amulet.safeTransferFrom(address(this), msg.sender, request.tokenId);
+    }
 
-        delete requests[_requestId];
+    // Handle ERC721 token reception and register members or withdrawal requests
+    function onERC721Received(
+        address,
+        address _from,
+        uint256 _tokenId,
+        bytes calldata _data
+    ) external override returns (bytes4) {
+        require(msg.sender == address(amulet), "Only Amulets allowed");
+        require(_tokenId >= 0, "Invalid token");
+
+        if (!isMemberMap[_from]) {
+            members.push(_from);
+            isMemberMap[_from] = true;
+        } else if (_data.length > 0) {
+            uint256 withdrawTokenId = abi.decode(_data, (uint256));
+            withdrawRequests[nextRequestId] = WithdrawRequest({
+                requester: _from,
+                tokenId: withdrawTokenId,
+                votes: 1,
+                completed: false
+            });
+            emit WithdrawRequested(_from, withdrawTokenId, nextRequestId);
+            hasVoted[_from][nextRequestId] = true;
+            nextRequestId++;
+        }
+
+        return this.onERC721Received.selector;
     }
 }

b9da46c2cd71232cd8e06ffd385a6653b4c688c7
boobaGreen <claudiodallara77@gmail.com>
2024-09-21 01:18:11 +0200

    proxy old

--- campaigns/proxy-contracts/basic-proxies/contracts/UpgradeableMechSuit.sol
+++ campaigns/proxy-contracts/basic-proxies/contracts/UpgradeableMechSuit.sol
@@ -2,38 +2,18 @@
 pragma solidity ^0.8.19;
 
 contract UpgradeableMechSuit {
-
-    bytes32 internal  _IMPLEMENTATION_SLOT  = bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1);
-
-    bytes32 internal constant _ADMIN_SLOT = bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1);
-
-    function _getAddress(bytes32 slot) public view returns (address addr) {
-        assembly {
-            addr := sload(slot)
-        }
-    return addr;
-    }
-    function _setAddress(bytes32 slot, address addr) public {
-        assembly {
-            sstore(slot, addr)
-        }
-    }
-
+    address public implementation;
 
     /// @notice Constructs the contract
-    /// @param implementation Address of logic contract to be linked
-    constructor (address implementation) {
-        _setAddress(_ADMIN_SLOT, msg.sender);
-        _setAddress(_IMPLEMENTATION_SLOT, implementation);
+    /// @param _implementation Address of logic contract to be linked
+    constructor(address _implementation) {
+        implementation = _implementation;
     }
 
     /// @notice Upgrades contract by updating the linked logic contract
-    /// @param implementation Address of new logic contract to be linked
-    function upgradeTo(address implementation) external {
-        address admin = _getAddress(_ADMIN_SLOT);
-        require(msg.sender == admin, "UpgradeableMechSuit: only admin can upgrade");
-        _setAddress(_IMPLEMENTATION_SLOT, implementation);
-        
+    /// @param _implementation Address of new logic contract to be linked
+    function upgradeTo(address _implementation) external {
+        implementation = _implementation;
     }
 
     /// @notice Fallback function to delegate calls to the implementation contract
@@ -46,7 +26,7 @@ contract UpgradeableMechSuit {
             calldatacopy(ptr, 0, calldatasize())
 
             // (2) Forward call to implementation contract
-            let result := delegatecall(gas(), sload(_IMPLEMENTATION_SLOT.slot), ptr, calldatasize(), 0, 0)
+            let result := delegatecall(gas(), sload(implementation.slot), ptr, calldatasize(), 0, 0)
             let size := returndatasize()
 
             // (3) Retrieve return data

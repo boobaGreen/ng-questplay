255037101b891cc0735f43e46f6957f05ccc11f5
boobaGreen <claudiodallara77@gmail.com>
2024-09-23 20:37:31 +0200

    memory pop push4

--- campaigns/learning-assembly/memory-manipulation/contracts/MemoryLayout.sol
+++ campaigns/learning-assembly/memory-manipulation/contracts/MemoryLayout.sol
@@ -33,7 +33,7 @@ contract MemoryLayout {
         }
     }
 
-    /// @notice Create a bytes memory array.
+     /// @notice Create a bytes memory array.
     /// @param size The size of the array.
     /// @param value The initial value of each element of the array.
     function createBytesArray(
@@ -47,21 +47,17 @@ contract MemoryLayout {
             // 2. Record the length of the array (in bytes, not words)
             mstore(array, size)
 
-            // 3. Initialize next `size` bytes with `value`.
+            // 3. Initialize next `size` bytes with `value`
             // Starting offset is 0x20 (skipping the length field)
-            let offset := 0x20
+            let offset := add(array, 0x20)
 
-            // Prepare the `bytes1` value to be left-aligned
-            let leftAlignedValue := shl(248, value)  // shift 248 bits to the left to align `bytes1`
-
-            // Initialize the content of the array to the left-aligned value
+            // Initialize the content of the array to the given value
             for {let i := 0} lt(i, size) {i := add(i, 1)} {
-                mstore8(add(array, offset), leftAlignedValue)
-                offset := add(offset, 1)
+                mstore8(add(offset, i), value)
             }
 
             // 4. Mark the array memory area as allocated
-            mstore(0x40, add(array, add(0x20, size)))
+            mstore(0x40, add(offset, size))
         }
     }
 }

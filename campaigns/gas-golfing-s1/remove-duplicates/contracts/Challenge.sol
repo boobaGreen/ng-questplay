2d158394c4ca1996a5037933a90a6dc658f8208b
boobaGreen <claudiodallara77@gmail.com>
2024-09-23 21:20:07 +0200

    golf gas 1

--- campaigns/gas-golfing-s1/remove-duplicates/contracts/Challenge.sol
+++ campaigns/gas-golfing-s1/remove-duplicates/contracts/Challenge.sol
@@ -10,6 +10,23 @@ contract Challenge {
      */
     function dispelDuplicates(
         uint8[] calldata input
-    ) public pure returns (uint8[] memory output) { }
-    
+    ) public pure returns (uint8[] memory output) {
+        uint8[] memory temp = new uint8[](input.length);
+        bool[256] memory seen; // Vettore per tenere traccia dei valori già visti
+        uint8 uniqueIndex = 0;
+
+        for (uint8 i = 0; i < input.length; i++) {
+            if (!seen[input[i]]) {
+                seen[input[i]] = true; // Marca il numero come visto
+                temp[uniqueIndex] = input[i];
+                uniqueIndex++;
+            }
+        }
+
+        // Crea l'array finale della dimensione corretta
+        output = new uint8[](uniqueIndex);
+        for (uint8 i = 0; i < uniqueIndex; i++) {
+            output[i] = temp[i];
+        }
+    }
 }

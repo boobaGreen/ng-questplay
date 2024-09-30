7e20028f51792f183318850130a570e94b44c2fb
boobaGreen <claudiodallara77@gmail.com>
2024-09-30 14:27:10 +0200

    delegate call detection

--- campaigns/standalone-quests/delegate-call-detection/contracts/Mind.sol
+++ campaigns/standalone-quests/delegate-call-detection/contracts/Mind.sol
@@ -2,12 +2,16 @@
 pragma solidity ^0.8.19;
 
 contract Mind {
+    address immutable deploymentAddress ;
     /// @notice This could be useful...
-    constructor() {}
+    constructor() {
+        deploymentAddress = address(this);
+    }
 
     /// @notice Checks if the current call is a delegate call.
     /// @return isDelegate true if this is a delegate call, false otherwise.
     function isDelegateCall() public view returns (bool isDelegate) {
-        
+         // Se address(this) è diverso dall'indirizzo di distribuzione, è una delegate call
+        return address(this) != deploymentAddress;
     }
 }
